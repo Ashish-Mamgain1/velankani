abb5bf2dc46192e15e354f7df6022769d34ce3bb
ashish.mamgain <ashish.mamgain@D464.blr.velankani.com>
2020-01-09 17:05:31 +0530

    Threads Concepts
    Change-Id: Ic3df9d4245e47c386260da2b7c7c1fe6fb1c3f6a

---
+++ JavaBasics/CoreJava/src/com/velankani/Threads/DaemonThread.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.velankani.Threads;
+
+/**
+ * @author ${Ashish Mamgain}
+ * 09-Jan-2020
+ */
+
+// Java program to demonstrate the usage of
+// setDaemon() and isDaemon() method.
+public class DaemonThread extends Thread {
+	public DaemonThread(String name) {
+		super(name);
+	}
+
+	public void run() {
+		// Checking whether the thread is Daemon or not
+		if (Thread.currentThread().isDaemon()) {
+			System.out.println(getName() + " is Daemon thread");
+		}
+
+		else {
+			System.out.println(getName() + " is User thread");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		DaemonThread t1 = new DaemonThread("t1");
+		DaemonThread t2 = new DaemonThread("t2");
+		DaemonThread t3 = new DaemonThread("t3");
+
+		// Setting user thread t1 to Daemon
+		t1.setDaemon(true);
+
+		// starting first 2 threads
+		t1.start();
+		t2.start();
+
+		// Setting user thread t3 to Daemon
+		t3.setDaemon(true);
+		t3.start();
+	}
+}
