0047ffb1ad8948a96d13bdb176d224f3432ce0e1
“Ashish-Mamgain1” <ashish.mamgain@velankani.com>
2020-01-31 15:19:12 +0530

    Added data

---
+++ AdvanceJava/spring-mvc-demo/src/com/learn/SpringMVC/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.learn.SpringMVC;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		String theName = request.getParameter("studentName"); 
+		System.out.println("*********************theName : "+theName);
+		theName = theName.toUpperCase();
+		String result = "Yo "+ theName;
+		model.addAttribute("message", result);
+		System.out.println("*********************model : "+model);
+		return "helloworld";
+	}
+
+}
