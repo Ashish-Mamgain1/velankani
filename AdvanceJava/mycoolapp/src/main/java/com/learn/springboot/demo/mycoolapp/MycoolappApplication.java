058532b9e6e2ca0a2def27362e87260d4f3eb68e
Ashish Mamgain <ashish.mamgain@blr.velankani.com>
2020-02-06 17:53:09 +0530

    Adding Code to the Repo

--- AdvanceJava/mycoolapp/src/main/java/com/learn/springboot/demo/mycoolapp/MycoolappApplication.java
+++ AdvanceJava/mycoolapp/src/main/java/com/learn/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -2,8 +2,11 @@ package com.learn.springboot.demo.mycoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+//@SpringBootApplication(scanBasePackages = {"com.learn.springboot.demo.rest"})
+@SpringBootApplication()
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
---
+++ AdvanceJava/mycoolapp/src/main/java/com/learn/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,26 @@
+package com.learn.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/")
+	public String sayHello() {
+
+		return "Hi World! Time on Server is" + coachName + " "+teamName ;
+
+	}
+
+}
