940fb4ed1aa47a3b1b70ab9aabd45509408cda8f
Ashish Mamgain <ashish.mamgain@blr.velankani.com>
2020-02-10 16:52:03 +0530

    Adding more endpoints

---
+++ AdvanceJava/cruddemo/src/main/java/com/learn/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,53 @@
+package com.learn.springboot.cruddemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.learn.springboot.cruddemo.dao.EmployeeDAO;
+import com.learn.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO employeeDAO;
+
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+
+		this.employeeDAO = theEmployeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+		employeeDAO.save(theEmployee);
+
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		employeeDAO.deleteById(theId);
+
+	}
+
+}
