dfdd35ccff3d6ea2bbf26ac8930f7aa265bc191e
Ashish Mamgain <ashish.mamgain@blr.velankani.com>
2020-02-11 09:09:37 +0530

    Adding new project

---
+++ AdvanceJava/Boot-hibernate-with-service-demo/src/main/java/com/learn/springboot/Boothibernatewithservicedemo/BootHibernateWithServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.Boothibernatewithservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHibernateWithServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHibernateWithServiceDemoApplication.class, args);
+	}
+
+}
