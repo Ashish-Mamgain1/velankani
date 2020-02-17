13c8984bb09660eb055045b43ef4a98e46e4e118
Ashish Mamgain <ashish.mamgain@blr.velankani.com>
2020-02-17 14:50:06 +0530

    added microservices

---
+++ AdvanceJava/Microservices/currency-exchange-service/src/main/java/com/learn/microservices/currencyexchangeservice/controller/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.learn.microservices.currencyexchangeservice.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	
+	ExchangeValue findByFromAndTo(String from, String to);
+
+}
